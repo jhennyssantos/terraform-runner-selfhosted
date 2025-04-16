98fbd23ef4f87ba7819a5ba5364873bb15dadd37
jhennyssantos <jhenifersantos.si@gmail.com>
2025-04-15 22:48:00 -0300

    detroy resources

--- runner-ephemeral/main.tf
+++ runner-ephemeral/main.tf
@@ -1,70 +1,70 @@
-locals {
-  # Set the default region to us-east-1
-  aws_region = "us-east-1"
-  environment = "lab"
-}
+# locals {
+#   # Set the default region to us-east-1
+#   aws_region = "us-east-1"
+#   environment = "lab"
+# }
 
 
-resource "random_id" "random" {
-  byte_length = 20
-}
+# resource "random_id" "random" {
+#   byte_length = 20
+# }
 
-module "runners" {
-  source  = "github-aws-runners/github-runner/aws"
-  version = "6.1.0"
-  create_service_linked_role_spot = true
-  aws_region                      = var.aws_region
-  vpc_id                          = var.vpc_id
-  subnet_ids                      = var.subnet_ids
+# module "runners" {
+#   source  = "github-aws-runners/github-runner/aws"
+#   version = "6.1.0"
+#   create_service_linked_role_spot = true
+#   aws_region                      = var.aws_region
+#   vpc_id                          = var.vpc_id
+#   subnet_ids                      = var.subnet_ids
 
 
-  github_app = {
-    key_base64     = var.key_base64
-    id             = var.app_id
-    webhook_secret = random_id.random.hex
-  }
+#   github_app = {
+#     key_base64     = var.key_base64
+#     id             = var.app_id
+#     webhook_secret = random_id.random.hex
+#   }
 
-  # When not explicitly set lambda zip files are grapped from the module requiring lambda build.
-  # Alternatively you can set the path to the lambda zip files here.
-  #
-  # For example grab zip files via lambda_download
-  webhook_lambda_zip                = "../lambdas-download/webhook.zip"
-  runner_binaries_syncer_lambda_zip = "../lambdas-download/runner-binaries-syncer.zip"
-  runners_lambda_zip                = "../lambdas-download/runners.zip"
+#   # When not explicitly set lambda zip files are grapped from the module requiring lambda build.
+#   # Alternatively you can set the path to the lambda zip files here.
+#   #
+#   # For example grab zip files via lambda_download
+#   webhook_lambda_zip                = "../lambdas-download/webhook.zip"
+#   runner_binaries_syncer_lambda_zip = "../lambdas-download/runner-binaries-syncer.zip"
+#   runners_lambda_zip                = "../lambdas-download/runners.zip"
 
-  runner_extra_labels         = ["default"]
+#   runner_extra_labels         = ["default"]
 
-  # enable access to the runners via SSM
-  enable_ssm_on_runners = true
+#   # enable access to the runners via SSM
+#   enable_ssm_on_runners = true
 
-  # Let the module manage the service linked role
-  # create_service_linked_role_spot = true
+#   # Let the module manage the service linked role
+#   # create_service_linked_role_spot = true
 
-  instance_types = ["t2.micro"]
+#   instance_types = ["t2.micro"]
 
-  # override delay of events in seconds
-  delay_webhook_event = 0
+#   # override delay of events in seconds
+#   delay_webhook_event = 0
 
-  # Ensure you set the number not too low, each build require a new instance
-  runners_maximum_count = 2
+#   # Ensure you set the number not too low, each build require a new instance
+#   runners_maximum_count = 2
 
-  # override scaling down
-  scale_down_schedule_expression = "cron(* * * * ? *)"
+#   # override scaling down
+#   scale_down_schedule_expression = "cron(* * * * ? *)"
 
-  enable_ephemeral_runners = true
+#   enable_ephemeral_runners = true
 
-  # # Example of simple pool usages
-  # pool_runner_owner = "philips-test-runners"
-  # pool_config = [{
-  #   size                         = 3
-  #   schedule_expression = "cron(0/3 14 * * ? *)" # every 3 minutes between 14:00 and 15:00
-  #   schedule_expression_timezone = "Europe/Amsterdam"
+#   # # Example of simple pool usages
+#   # pool_runner_owner = "philips-test-runners"
+#   # pool_config = [{
+#   #   size                         = 3
+#   #   schedule_expression = "cron(0/3 14 * * ? *)" # every 3 minutes between 14:00 and 15:00
+#   #   schedule_expression_timezone = "Europe/Amsterdam"
 
-  # }]
-  #
-  #
-  enable_job_queued_check = true
-  enable_organization_runners = true
+#   # }]
+#   #
+#   #
+#   enable_job_queued_check = true
+#   enable_organization_runners = true
   
   
 
